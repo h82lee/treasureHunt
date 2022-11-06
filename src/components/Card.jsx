ce24af6d2725dfe46628eea4722a2dff0ffa51ba
Hyeran Lee <hyeranlee@MacBook-Pro-2.local>
2022-11-06 12:06:20 -0500

    add lowercase

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,62 +1,71 @@
-import React, { useState } from 'react';
-import { Typography, CardContent, CardActions, Box, Card, Input, Grid, Fab } from '@mui/material';
-import {useStyles} from "./styles";
+import React, { useState } from "react";
+import {
+  Typography,
+  CardContent,
+  CardActions,
+  Box,
+  Card,
+  Input,
+  Grid,
+  Fab,
+  TextField,
+} from "@mui/material";
+import { useStyles } from "./styles";
 
-
-function Textbox(props) { 
-
-
+function Textbox(props) {
   const classes = useStyles();
 
-  const [writtenPassword, setWrittenPassword]= useState("")
+  const [writtenPassword, setWrittenPassword] = useState("");
 
-  function handleChange(event){
-    const {value} = event.target
-    setWrittenPassword(value)
+  function handleChange(event) {
+    const { value } = event.target;
+    const lowerCasedValue = value.toLowerCase();
+    setWrittenPassword(lowerCasedValue);
   }
 
-
   return (
-  <div>
-  <Grid container justifyContent="center" className={classes.grid}>
-   <Box component ="form" className={classes.textbox}>
-   <Card sx={{ width:500 }}>
-      <CardContent >
-      <div className={classes.text}>        
-      <Typography variant="h5" gutterBottom align="center" >
-          Hint #{props.hintNumber}
-        </Typography>
-        <Typography variant="inherit" align="center" gutterBottom>
-        {props.hintDescription} 
-        </Typography>
-        </div>
-      </CardContent>
-      <CardActions className={classes.card}>
-      <Grid container spacing ={4} justifyContent="center">
-      <Grid item>
-      <Input name="password"
-        placeholder='password?'
-        value={writtenPassword}
-        onChange={handleChange}
-        >
-        </Input>
+    <div>
+      <Grid container justifyContent="center" className={classes.grid}>
+        <Box component="form" className={classes.textbox}>
+          <Card sx={{ width: 500 }}>
+            <CardContent>
+              <div className={classes.text}>
+                <Typography variant="h5" gutterBottom align="center">
+                  Hint #{props.hintNumber}
+                </Typography>
+                <Typography variant="inherit" align="center" gutterBottom>
+                  {props.hintDescription}
+                </Typography>
+              </div>
+            </CardContent>
+            <CardActions className={classes.card}>
+              <Grid container spacing={4} justifyContent="center">
+                <Grid item>
+                  <Input
+                    name="password"
+                    placeholder="password?"
+                    value={writtenPassword}
+                    onChange={handleChange}
+                  ></Input>
+                </Grid>
+                <Grid item>
+                  <Fab
+                    onClick={(e) => {
+                      e.preventDefault();
+                      props.handleGoClick(writtenPassword);
+                    }}
+                    size="small"
+                    color="primary"
+                  >
+                    Go
+                  </Fab>
+                </Grid>
+              </Grid>
+            </CardActions>
+          </Card>
+        </Box>
       </Grid>
-      <Grid item>
-      <Fab 
-        onClick={(e)=>{
-        e.preventDefault();
-        props.handleGoClick(writtenPassword)
-        }} size="small"
-        color="primary">
-        Go
-        </Fab>
-      </Grid>
-        </Grid>
-      </CardActions>
-      </Card>
-    </Box>
-    </Grid>
-  </div>
+    </div>
   );
 }
-export default Textbox
+export default Textbox;
--- src/hints.js
+++ src/hints.js
@@ -1,29 +1,36 @@
-const hints =[
-    {id:1,
+const hints = [
+  {
+    id: 1,
     description: "The more I dry, the wetter I become. What am I?",
-    password:"895207"
-    // towel
-    },
-    {id:2,
+    password: "towel",
+    // 895207
+  },
+  {
+    id: 2,
     description: "What one word has the most letters in it?",
-    password:"412704"
-    // Mailbox
-    },
-    {id:3,
-    description: "Sitff is my spine and my body is pale, but I'm always ready to tell. What am I?",
-    password:"133195"
-    //Book
-    },
-    {id:4,
-    description: "You use a knife to slice my head and weep beside me when I am dead. What am I?",
-    password:"256569"
-    //Onion
-    },
-    {id:5,
+    password: "mailbox",
+    // 412704
+  },
+  {
+    id: 3,
+    description:
+      "Sitff is my spine and my body is pale, but I'm always ready to tell. What am I?",
+    password: "book",
+    //133195
+  },
+  {
+    id: 4,
+    description:
+      "You use a knife to slice my head and weep beside me when I am dead. What am I?",
+    password: "onion",
+    //256569
+  },
+  {
+    id: 5,
     description: "If you were 'able', you'd add a 'T'",
-    password:"591962"
-    //Table
-    }
-]
+    password: "table",
+    //591962
+  },
+];
 
-export default hints
+export default hints;
